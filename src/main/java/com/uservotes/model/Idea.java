a51d88577cbfe6c957be5994aff1969bd5273e03
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-09-28 19:29:09 -0700

    implementing slugs for searching

--- src/main/java/com/uservotes/model/Idea.java
+++ src/main/java/com/uservotes/model/Idea.java
@@ -1,14 +1,24 @@
 package com.uservotes.model;
 
+import com.github.slugify.Slugify;
+
+import java.io.IOException;
 import java.util.Objects;
 
 public class Idea {
+    private String slug;
     private String title;
     private String creator;
 
     public Idea(String title, String creator) {
         this.title = title;
         this.creator = creator;
+        try {
+            Slugify slugify = new Slugify();
+            slug = slugify.slugify(title);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getTitle() {
@@ -17,6 +27,10 @@ public class Idea {
 
     public String getCreator() {
         return creator;
+    }
+
+    public String getSlug() {
+        return slug;
     }
 
     @Override
