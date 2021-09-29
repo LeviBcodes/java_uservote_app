22e79923c50dcd48f322be7a08c5f4f425b96641
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2021-09-28 19:53:22 -0700

    adding voters

--- src/main/java/com/uservotes/Main.java
+++ src/main/java/com/uservotes/Main.java
@@ -54,5 +54,12 @@ public class Main {
             res.redirect("ideas");
             return null;
         });
+
+        post("/ideas/:slug/vote", (req, res) -> {
+            Idea idea = dao.findBySlug(req.params("slug"));
+            idea.addVoter(req.attribute("username"));
+            res.redirect("/ideas");
+            return null;
+        });
     }
 }
--- src/main/java/com/uservotes/model/Idea.java
+++ src/main/java/com/uservotes/model/Idea.java
@@ -3,14 +3,18 @@ package com.uservotes.model;
 import com.github.slugify.Slugify;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Idea {
     private String slug;
     private String title;
     private String creator;
+    private Set<String> voters;
 
     public Idea(String title, String creator) {
+        voters = new HashSet<>();
         this.title = title;
         this.creator = creator;
         try {
@@ -31,6 +35,14 @@ public class Idea {
 
     public String getSlug() {
         return slug;
+    }
+
+    public boolean addVoter(String voterUserName) {
+        return voters.add(voterUserName);
+    }
+
+    public int getVoteCount() {
+        return voters.size();
     }
 
     @Override
